0ba1c53d9f25c644835d1a6aaaa1c814ffec04a5
markeryjane <96929201+markeryjane@users.noreply.github.com>
2024-10-05 19:38:49 -0400

    bonus multipliers work on score

--- scenes/game_over_screen.gd
+++ scenes/game_over_screen.gd
@@ -10,29 +10,47 @@ var gameover_year_bonus_active = false
 @onready var spring_bonus_text: Label = %SpringBonusText
 @onready var summer_bonus_text: Label = %SummerBonusText
 @onready var autumn_bonus_text: Label = %AutumnBonusText
+@onready var year_bonus_text: Label = %YearBonusText
 
 @onready var score_text: Label = %ScoreText
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	score_text.text = str(final_score)
+	var bonus_multiplier = 0
+	
 	if gameover_spring_bonus_active:
+		#final_score *= 3
+		bonus_multiplier += 3
 		spring_bonus_text.text = "Spring bonus: x3"
 	else:
 		spring_bonus_text.modulate = Color.html("#ffffff32")
 	if gameover_summer_bonus_active:
+		#final_score *= 3
+		bonus_multiplier += 3
 		summer_bonus_text.text = "Summer bonus: x3"
 	else:
 		summer_bonus_text.modulate = Color.html("#ffffff32")
 	if gameover_autumn_bonus_active:
+		#final_score *= 3
+		bonus_multiplier += 3
 		autumn_bonus_text.text = "Autumn bonus: x3"
 	else:
 		autumn_bonus_text.modulate = Color.html("#ffffff32")
 	if gameover_winter_bonus_active:
+		#final_score *= 3
+		bonus_multiplier += 3
 		winter_bonus_text.text = "Winter bonus: x3"
 	else:
 		winter_bonus_text.modulate = Color.html("#ffffff32")
+	if gameover_year_bonus_active:
+		#final_score *= 5
+		bonus_multiplier += 5
+		year_bonus_text.text = "Year bonus: x5"
+	else:
+		year_bonus_text.modulate = Color.html("#ffffff32")
 	
+	final_score *= max(1,bonus_multiplier)
+	score_text.text = str(final_score)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
